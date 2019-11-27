a25ab35c51e7dea5c8b45761e6419a1f76a497c8
sushiMish <sushila.mishra12@gmail.com>
2019-11-28 01:25:55 +0530

    Add summary and description to the endpoints

--- src/main/kotlin/com/myservice/controller/PersonController.kt
+++ src/main/kotlin/com/myservice/controller/PersonController.kt
@@ -4,6 +4,7 @@ import com.myservice.controller.dto.Person
 import com.myservice.service.PersonService
 import com.github.fge.jsonpatch.JsonPatch
 import com.myservice.controller.dto.CreateResponse
+import io.swagger.v3.oas.annotations.Operation
 import org.reactivestreams.Publisher
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
@@ -19,16 +20,19 @@ class PersonController {
     lateinit var service: PersonService
 
     @GetMapping(value = ["/{id}"], produces = ["application/json"])
+    @Operation(summary = "Find a person", description = "Finds a person with the id")
     fun find(@PathVariable id: UUID): Mono<Person> {
         return service.findPerson(id)
     }
 
     @GetMapping(produces = ["application/json"])
+    @Operation(summary = "List persons", description = "List all the people")
     fun list(): Flux<Person> {
         return service.findAll()
     }
 
     @PostMapping(consumes = ["application/json"], produces = ["application/json"])
+    @Operation(summary = "Create a person", description = "Creates a person with inputs")
     fun create(@RequestBody personInput:  Mono<Person>): Mono<CreateResponse> {
         return service.createPerson(personInput).map {
             CreateResponse(it)
@@ -36,6 +40,7 @@ class PersonController {
     }
 
     @PatchMapping(value = ["/{id}"], consumes = ["application/json"], produces = ["application/json"])
+    @Operation(summary = "Update a person", description = "Updates a person using patch")
     fun patch(@PathVariable id: UUID, @RequestBody personPatch: JsonPatch): Mono<Person> {
         return service.patchPerson(id, personPatch)
     }
