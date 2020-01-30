64b874bb1cf7b4c7297d890c2d0f6ffea9fe14bc
vaibhawj <vj.vaibhaw@gmail.com>
2020-01-30 20:39:17 +0530

    Add integration test

--- src/test/kotlin/com/myservice/controller/PersonControllerTest.kt
+++ src/test/kotlin/com/myservice/controller/PersonControllerTest.kt
@@ -1,26 +1,37 @@
 package com.myservice.controller
 
-import com.myservice.service.PersonService
-import io.mockk.every
-import io.mockk.mockk
+import com.myservice.MyServiceApplication
+import org.cassandraunit.utils.EmbeddedCassandraServerHelper
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import reactor.core.publisher.toMono
-import reactor.test.StepVerifier
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.reactive.server.WebTestClient
 import java.util.*
 
+@ExtendWith(SpringExtension::class)
+@SpringBootTest(classes = [MyServiceApplication::class])
+@AutoConfigureWebTestClient
 internal class PersonControllerTest {
 
+    @Autowired
+    private lateinit var client: WebTestClient
+
+    @BeforeEach
+    fun setup() {
+        EmbeddedCassandraServerHelper.startEmbeddedCassandra()
+    }
+
     @Test
-    fun find() {
-        val service = mockk<PersonService>()
-        val controller = PersonController()
-        controller.service = service
-
-        val id = UUID.randomUUID()
-        every { service.findPerson(id) }.returns(com.myservice.controller.dto.Person(id = id, firstName = "John").toMono())
-
-        StepVerifier.create(controller.find(id))
-                .expectNextMatches { it.body?.firstName == "John" }
-                .verifyComplete()
+    fun test() {
+        client.get().uri("/persons/${UUID.randomUUID()}")
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Content-Type", "application/json")
+                .exchange()
+                .expectStatus().isNotFound
     }
 }
